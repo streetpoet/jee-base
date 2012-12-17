229830f868585f2e33f2891b708567b805f7f80e
streetpoet <streetpoet@163.com>
2012-12-17 13:47:40 +0800

    change to use @Resource annotation to retrieve connection pool

--- love-web/src/com/spstudio/love/web/system/LoveDaemon.java
+++ love-web/src/com/spstudio/love/web/system/LoveDaemon.java
@@ -1,12 +1,12 @@
 package com.spstudio.love.web.system;
 
+import javax.annotation.Resource;
+import javax.annotation.Resource.AuthenticationType;
 import javax.faces.application.Application;
 import javax.faces.application.ProjectStage;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.jboss.logging.Logger;
@@ -17,8 +17,13 @@ public class LoveDaemon {
 	
 	private Logger log = Logger.getLogger(LoveDaemon.class);
 	
-	private static InitialContext CTX = null;
-	private static DataSource DATASOURCE = null;
+	@Resource(name = "LoveWebDB",
+			mappedName = "java:/LoveWebDB",
+			authenticationType = AuthenticationType.CONTAINER, 
+			shareable = false, 
+			description = "datasource of connection pool")
+	private static javax.sql.DataSource DATASOURCE = null;
+	
 	
 	public LoveDaemon(){
 		log.info("LoveDaemon started.");
@@ -30,30 +35,9 @@ public class LoveDaemon {
 		if (application.getProjectStage() == ProjectStage.Development){
 			log.warn("Project is in development stage.");
 		}
-		
-		/*
-		 * 
-		 */
-		try{
-			CTX = new InitialContext();
-		}catch(NamingException e){
-			log.error("Can't create InitialContext Object.", e);
-		}
-		
-		/*
-		 * 
-		 */
-		try{
-			DATASOURCE = (DataSource)CTX.lookup("java:LoveWebDB");
-		}catch(NamingException e){
-			log.error("Can't lookup jndi database.", e);
-		}
 	}
 	
 	public static DataSource getDataSource(){
-		if (DATASOURCE != null){
-			return DATASOURCE;
-		}
-		return null;
+		return DATASOURCE;
 	}
 }

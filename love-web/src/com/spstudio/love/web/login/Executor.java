9a3dc0941250c42364d566ee77727eae5d581962
sp <sp@127.0.0.1>
2012-12-18 10:47:21 +0800

    draft submit

--- love-web/src/com/spstudio/love/web/login/Executor.java
+++ love-web/src/com/spstudio/love/web/login/Executor.java
@@ -1,7 +1,17 @@
 package com.spstudio.love.web.login;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginContext;
 
 import org.jboss.logging.Logger;
 
@@ -16,8 +26,38 @@ public class Executor {
 	
 	public Object doExecute(){
 		logger.trace("### doExecute");
-		new DatabaseHelper().doQuery("select * from sp");
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 		return StandardNavigation.SUCCESS;
 	}
 	
+	public void logout(){
+		
+		LoginContext lc = null;
+		try{
+			lc = new LoginContext("love-web", new CallbackHandler() {
+				
+				@Override
+				public void handle(Callback[] callBack) throws IOException,
+						UnsupportedCallbackException {
+					for (Callback c: callBack){
+						if (c instanceof NameCallback){
+							NameCallback nameCallback = (NameCallback)c;
+							nameCallback.setName("streetpoet");
+						}else if (c instanceof PasswordCallback){
+							PasswordCallback passwordCallback = (PasswordCallback)c;
+							passwordCallback.setPassword("213231".toCharArray());
+						}else {
+							throw new UnsupportedCallbackException(c, "Unrecognized Callback");
+						}
+					}
+				}
+			});
+			lc.login();
+			Subject loginSubject = lc.getSubject();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+	}
 }

6d68515a0ee363a07a4f0db07b344bf73ff41be4
sp <sp@127.0.0.1>
2013-01-02 08:39:27 +0800

    can be invoked using app client mode

--- love-ejb/ejbModule/impl/ProductsBean.java
+++ love-ejb/ejbModule/impl/ProductsBean.java
@@ -2,11 +2,25 @@ package impl;
 
 import interfaces.IProducts;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 @Stateless
+@Remote
 public class ProductsBean implements IProducts {
 
+	@PostConstruct
+	public void postConstruct(){
+		System.out.println("ProductsBean#postConstruct");
+	}
+	
+	@PreDestroy
+	public void preDestroy(){
+		System.out.println("ProductsBean#preDestroy");
+	}
+	
 	@Override
 	public void queryProducts(int kindId, int maxCount) {
 		System.out.println("print nothing");
--- love-ejb/ejbModule/interfaces/IProducts.java
+++ love-ejb/ejbModule/interfaces/IProducts.java
@@ -1,8 +1,6 @@
 package interfaces;
 
-import javax.ejb.Remote;
 
-@Remote
 public interface IProducts {
 	public void queryProducts(int kindId, int maxCount);
 }

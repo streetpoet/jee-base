5c3f087ac613f19b86db5d6bf6e5afdb6aecd52f
sp <sp@127.0.0.1>
2013-01-04 14:43:25 +0800

    change method name

--- love-web/src/com/spstudio/love/web/interceptor/LoveLoggedInteceptor.java
+++ love-web/src/com/spstudio/love/web/interceptor/LoveLoggedInteceptor.java
@@ -14,7 +14,7 @@ public class LoveLoggedInteceptor {
 	private Logger log = Logger.getLogger(LoveLoggedInteceptor.class);
 	
 	@AroundInvoke
-	public java.lang.Object loadFamily(javax.interceptor.InvocationContext context) throws Exception{
+	public java.lang.Object invoke(javax.interceptor.InvocationContext context) throws Exception{
 		log.info(context.getClass().getName() + "#" + context.getMethod() + "#" + context.getTarget());
 		return context.proceed();
 	}

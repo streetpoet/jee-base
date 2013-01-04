1d673a51fc974d67fc6fc3a3cc4be5e93c7ffa8a
sp <sp@127.0.0.1>
2013-01-04 18:28:46 +0800

    add ProductSingletonEJB quanlifier and producer

---
+++ love-web/src/com/spstudio/love/web/producer/ProductSingletonEJBProducer.java
@@ -0,0 +1,31 @@
+package com.spstudio.love.web.producer;
+
+import interfaces.IProductSingleton;
+
+import javax.enterprise.inject.Produces;
+import javax.naming.Context;
+import javax.naming.NamingException;
+
+import com.spstudio.love.web.qualifiers.LoveLogged;
+import com.spstudio.love.web.qualifiers.ProductSingletonEJB;
+import com.spstudio.love.web.system.LoveDaemon;
+
+public class ProductSingletonEJBProducer {
+	
+	@SuppressWarnings("unused")
+	@Produces
+	@ProductSingletonEJB
+	@LoveLogged
+	private IProductSingleton produce(){
+		IProductSingleton products = null;
+ 		try {
+ 			LoveDaemon.ic.addToEnvironment(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+ 			LoveDaemon.ic.addToEnvironment(Context.PROVIDER_URL, "jnp://localhost:1099");
+ 			LoveDaemon.ic.addToEnvironment(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
+ 			products = (IProductSingleton)LoveDaemon.ic.lookup("ProductSingletonBean/remote");
+ 		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+ 		return products;
+	}
+}

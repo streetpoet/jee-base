4c0813447bf339a4853c95ea469085047a5d30f3
sp <sp@127.0.0.1>
2013-01-11 10:31:54 +0800

    add logic

--- love-ejb/ejbModule/impl/ProductBean.java
+++ love-ejb/ejbModule/impl/ProductBean.java
@@ -3,6 +3,7 @@ package impl;
 import interfaces.IProduct;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -25,7 +26,7 @@ public class ProductBean implements IProduct {
 			"values(?, ?, ?, ?, ?, ?, ?, ?)";
 	private static final String QUERY_PRODUCTS_SQL_SUB_1 = "select p.id, c.kindName, u.nickname, productName, price, buyDate, warrantyEndDate, description";
 	private static final String QUERY_PRODUCTS_SQL_SUB_2 = "select count(0) as count";
-	private static final String QUERY_PRODUCTS_SQL_SUB_FROM = " FROM lovedb.f1_products p, f1_classify c, users u where p.classifyId = c.id and p.forUserId = u.id order by buyDate desc limit ?, ?";
+	private static final String QUERY_PRODUCTS_SQL_SUB_3 = " FROM lovedb.f1_products p, f1_classify c, users u where p.classifyId = c.id and p.forUserId = u.id order by buyDate desc limit ?, ?";
 	
 	@Override
 	public boolean addProduct(Product product) {
@@ -46,15 +47,32 @@ public class ProductBean implements IProduct {
 	public IQueryResult<Product> queryProducts(ProductCondition condition,
 			PageObject pageObject) {
 		IQueryResult<Product> returnResult = new ProductQueryResult();
+		
 		Object[] params = new Object[] {
 			pageObject.getOffset(),
 			pageObject.getRecordCountPerFetch()
 		};
-		List<Object[]> result = helper.doQuery(QUERY_PRODUCTS_SQL_SUB_2 + QUERY_PRODUCTS_SQL_SUB_FROM, params);
-		List<Product> listProduct = new ArrayList<Product>();
-		pageObject.setTotalRecordsNumber((Long)result.get(0)[0]);
-		((ProductQueryResult)returnResult).setListProduct(listProduct);
+		List<Object[]> result = helper.doQuery(QUERY_PRODUCTS_SQL_SUB_2 + QUERY_PRODUCTS_SQL_SUB_3, params);
+		if (result != null && result.size() != 0){
+			pageObject.setTotalRecordsNumber((Long)result.get(0)[0]);
+		}
 		((ProductQueryResult)returnResult).setPageObject(pageObject);
+		
+		result = helper.doQuery(QUERY_PRODUCTS_SQL_SUB_1 + QUERY_PRODUCTS_SQL_SUB_3, params);
+		List<Product> listProduct = new ArrayList<Product>();
+		if (result != null && result.size() != 0){
+			Product p = new Product();
+			for(Object[] row: result){
+				Product product = p.clone();
+				product.setProductName((String)row[1]);
+				product.setBuyDate((Date)row[5]);
+				product.setPrice((Integer)row[4]);
+				product.setWarrantyEndDate((Date)row[6]);
+				listProduct.add(product);
+			}
+		}
+		((ProductQueryResult)returnResult).setListProduct(listProduct);
+		
 		return returnResult;
 	}
 
